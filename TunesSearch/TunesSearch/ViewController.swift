8755d6f2df538e0685ecb77c85297f72874d3147
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-06-02 11:19:16 -0400

    add starter xcode project

---
+++ TunesSearch/TunesSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TunesSearch
+//
+//  Created by Julian A. Fordyce on 6/2/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
