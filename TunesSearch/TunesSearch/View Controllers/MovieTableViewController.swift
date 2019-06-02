29839d5442a9728bf9357c30afd9bdd499c31b40
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-06-02 12:17:31 -0400

    build out table view controller to display movies

--- TunesSearch/TunesSearch/View Controllers/MovieTableViewController.swift
+++ TunesSearch/TunesSearch/View Controllers/MovieTableViewController.swift
@@ -8,83 +8,59 @@
 
 import UIKit
 
+private let reuseIdentifier = "MediaCell"
+
 class MovieTableViewController: UITableViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        view.backgroundColor = .white
+        tableView.register(MediaTableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        setupTableView()
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        searchResultsController.fetchMovieResults { (results) in
+            guard let results = results else { return}
+            DispatchQueue.main.async {
+                self.results = results
+                self.tableView.reloadData()
+            }
+        }
     }
-
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return searchResultsController.results.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! MediaTableViewCell
+        
+        let test = searchResultsController.results[indexPath.row]
+        cell.nameLabel.text = test.name
+        cell.artistLabel.text = test.artistName
+        cell.genreLabel.text = test.genres[0].name
+        cell.releaseDateLabel.text = "Released on \(test.releaseDate)"
+        
+        guard let url = URL(string: test.artworkUrl100), let imageData = try? Data(contentsOf: url) else { return cell }
+        
+        cell.mediaImageView.image = UIImage(data: imageData)
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    
+    func setupTableView() {
+        tableView.rowHeight = 120
     }
-    */
+    
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
+    // MARK: - Properties
+    let searchResultsController = SearchResultsController()
+    var results: [Result] = []
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
