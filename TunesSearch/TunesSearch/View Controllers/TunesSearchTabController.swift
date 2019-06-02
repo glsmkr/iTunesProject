39d5bb3035c93f95504b528523f3c9b18c0d9995
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-06-02 12:00:49 -0400

    organize files into folders and made the tab bar controller the initial view controller

---
+++ TunesSearch/TunesSearch/View Controllers/TunesSearchTabController.swift
@@ -0,0 +1,30 @@
+//
+//  TunesSearchTabController.swift
+//  TunesSearch
+//
+//  Created by Julian A. Fordyce on 6/2/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import UIKit
+
+class TunesSearchTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
