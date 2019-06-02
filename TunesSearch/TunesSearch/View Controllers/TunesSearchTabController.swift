9a2d6b574a9b1653ce6304180edd451b4afe99db
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-06-02 12:05:01 -0400

    add table view controllers to the tab bar controller

--- TunesSearch/TunesSearch/View Controllers/TunesSearchTabController.swift
+++ TunesSearch/TunesSearch/View Controllers/TunesSearchTabController.swift
@@ -12,19 +12,19 @@ class TunesSearchTabController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let movieTVC = MovieTableViewController()
+        movieTVC.tabBarItem = UITabBarItem(tabBarSystemItem: .topRated, tag: 0)
+        let appsTVC = AppsTableViewController()
+        appsTVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        let musicTVC = MusicTableViewController()
+        musicTVC.tabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: 2)
+        
+        let viewControllerList = [musicTVC, appsTVC, movieTVC]
+        
+        viewControllers = viewControllerList.map { UINavigationController(rootViewController: $0) }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
