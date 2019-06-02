143b3b5ad2afe8605a45646f2f1051fd7ad33089
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-06-02 11:33:50 -0400

    create a model controller that handles all networking task

---
+++ TunesSearch/TunesSearch/SearchResultsController.swift
@@ -0,0 +1,41 @@
+//
+//  SearchResultsController.swift
+//  TunesSearch
+//
+//  Created by Julian A. Fordyce on 6/2/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import Foundation
+
+class SearchResultsController {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // MARK: - Properties
+    
+    var feed: Feed?
+    var results: [Result] = []
+    private let baseURL = URL(string: "https://rss.itunes.apple.com/api/v1")
+    
+    
+}
